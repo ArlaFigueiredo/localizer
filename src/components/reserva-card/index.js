04b78115412465b7f137b4d7d732e1c516427222
Arla Figueiredo <arla.figueiredo@gmail.com>
2021-12-20 11:19:36 -0300

    ajusta card minhas reservas

--- src/components/reserva-card/index.js
+++ src/components/reserva-card/index.js
@@ -1,9 +1,32 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { getStorage, ref, getDownloadURL } from 'firebase/storage'
+import { getFirestore, doc, addDoc, collection, getDoc } from 'firebase/firestore';
 
 
 function ReservaCard({id, veiculo, dataInicio, dataFim, status, total}) {
+
+    const [refVeiculo, setVeiculo] = useState([]);
+
+    async function fetchVeiculo() {
+        let db = getFirestore();
+        let docRef = doc(db, "veiculo", veiculo);
+        let veic = await getDoc(docRef);
+        let lista = [];
+        lista.push({
+            id: veic.id,
+            modelo: veic.data().modelo,
+            marca: veic.data().marca,
+            placa: veic.data().placa,
+            cor: veic.data().cor,
+            categoria: veic.data().categoria,
+        });
+        setVeiculo(lista[0]);
+    }
+
+    useEffect(() => {
+        fetchVeiculo();
+    }, []);
 
     return (
         <div className="card ml-2 mr-2 mb-4">
@@ -12,7 +35,7 @@ function ReservaCard({id, veiculo, dataInicio, dataFim, status, total}) {
                 <div className="row rodape-card d-flex align-items-center">
                     <h6>
                         Veiculo: 
-                        <span className="badge bg-success mb-3">{veiculo}</span>
+                        <span className="badge bg-success mb-3">{refVeiculo.modelo} - {refVeiculo.marca}/{refVeiculo.placa}</span>
                     </h6>
                 </div>
                 <div className="row rodape-card d-flex align-items-center">
@@ -28,7 +51,7 @@ function ReservaCard({id, veiculo, dataInicio, dataFim, status, total}) {
                 </div>
                 <div className="row rodape-card d-flex align-items-center">
                     
-                <h4>Valor Total:<span className="badge bg-warning ml-2 mb-3"> {total}</span></h4>
+                <h4>Valor Total:<span className="badge bg-warning ml-2 mb-3">R$ {total}</span></h4>
                 </div>
             </div>
         </div>
